fe15210f88f4b85f6efe2885f06ecfe8af857d55
Jereme <jeremejazz15@gmail.com>
2025-03-21 11:00:10 +0800

    fix: widget error and preload site to enable internet

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,24 @@
 import 'package:flutter/material.dart';
+// import 'package:http/http.dart' as http;
+
 import 'package:flutter_campus_map_demo/screens/home.dart';
 
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  // await _checkInternet();
   runApp(const MainApp());
 }
+
+// Future<void> _checkInternet() async {
+//   var url = Uri.https('olivarezmaps.netlify.app');
+//   // Await the http get response, then decode the json-formatted response.
+//   var response = await http.get(url);
+//   if (response.statusCode == 200) {
+//     debugPrint('Internet connection successful');
+//   } else {
+//     debugPrint('Request failed with status: ${response.statusCode}.');
+//   }
+// }
 
 class MainApp extends StatelessWidget {
   const MainApp({super.key});
--- lib/screens/webmap.dart
+++ lib/screens/webmap.dart
@@ -4,15 +4,17 @@ import 'package:flutter_inappwebview/flutter_inappwebview.dart';
 class WebMap extends StatelessWidget {
   final String url;
   final String title;
-  const WebMap({super.key, required this.title, required this.url});
+  const WebMap({
+    super.key,
+    required this.title,
+    this.url = 'https://olivarezmaps.netlify.app/maps/maps/olivarez/olivarez',
+  });
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text(title)),
-      body: Expanded(
-        child: InAppWebView(initialUrlRequest: URLRequest(url: WebUri(url))),
-      ),
+      body: InAppWebView(initialUrlRequest: URLRequest(url: WebUri(url))),
     );
   }
 }
